4aca132486219976543c9a569651dd162704aeef
Zarqu0n <zarquon.875@gmail.com>
2023-05-24 22:56:34 +0300

    Change parametric system

---
+++ Hex.sh
@@ -0,0 +1,47 @@
+#Physical properties
+    #Lengths in meters
+        export HEX_BASE_COXA_LENGTH=0.6
+        export HEX_COXA_LENGTH=0.6
+        export HEX_FEMUR_LENGTH=0.555
+        export HEX_TIBIA_LENGTH=0.755
+
+    # Masses in kg
+        export HEX_COVER_TOP_MASS=0.05
+        export HEX_BODY_TOP_MASS=0.2
+        export HEX_BOARD_BRACKET_MASS=0.3
+        export HEX_BODY_BOTTOM_MASS=0.2
+        export HEX_COXA_MASS=0.2
+        export HEX_FEMUR_MASS=0.2
+        export HEX_TIBIA_MASS=0.2
+
+    # Joints limits in radians
+        # export HEX_COXA_JOINT_LIMIT_LOWER=-0.707
+        # export HEX_COXA_JOINT_LIMIT_UPPER=0.707
+        # export HEX_FEMUR_JOINT_LIMIT_LOWER=-0.707
+        # export HEX_FEMUR_JOINT_LIMIT_UPPER=0.707
+        # export HEX_TIBIA_JOINT_LIMIT_LOWER=-2.3
+        # export HEX_TIBIA_JOINT_LIMIT_UPPER=0
+
+        export HEX_COXA_JOINT_LIMIT_LOWER=0
+        export HEX_COXA_JOINT_LIMIT_UPPER=6.28
+        export HEX_FEMUR_JOINT_LIMIT_LOWER=0
+        export HEX_FEMUR_JOINT_LIMIT_UPPER=6.28
+        export HEX_TIBIA_JOINT_LIMIT_LOWER=0
+        export HEX_TIBIA_JOINT_LIMIT_UPPER=6.28
+
+    # Joints stiffness in Nm/rad
+
+        export HEX_COXA_JOINT_DAMPING=60.0
+        export HEX_FEMUR_JOINT_DAMPING=60.0
+        export HEX_TIBIA_JOINT_DAMPING=0.7
+        export HEX_COXA_JOINT_FRICTION=4.0
+        export HEX_FEMUR_JOINT_FRICTION=4.0
+        export HEX_TIBIA_JOINT_FRICTION=0.1
+
+    # Joints effor and velocity
+        export HEX_BASE_COXA_EFFORT=50.0
+        export HEX_COXA_FEMUR_EFFORT=50.0
+        export HEX_FEMUR_TIBIA_EFFORT=50.0
+        export HEX_BASE_COXA_VELOCITY=1.0
+        export HEX_COXA_FEMUR_VELOCITY=1.0
+        export HEX_FEMUR_TIBIA_VELOCITY=1.0
