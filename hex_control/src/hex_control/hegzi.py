f995e1234fc40888fa1c4e97bd65c4f875ff3baf
Zarqu0n <zarquon.875@gmail.com>
2023-06-03 00:19:38 +0300

    Change all leg publisher

--- hex_control/src/hex_control/hegzi.py
+++ hex_control/src/hex_control/hegzi.py
@@ -36,18 +36,8 @@ class HexapodController:
             leg.setAngles(angle_array)
 
     def setAllLegsAngle(self,angle_array:list):
-        leg1_array = [angle_array[0],angle_array[5],angle_array[11]]
-        leg2_array = [angle_array[1],angle_array[6],angle_array[12]]
-        leg3_array = [angle_array[2],angle_array[7],angle_array[13]]
-        leg4_array = [angle_array[3],angle_array[8],angle_array[14]]
-        leg5_array = [angle_array[4],angle_array[9],angle_array[15]]
-        leg6_array = [angle_array[5],angle_array[10],angle_array[16]]
-        self.legs[0].setAngles(leg1_array)
-        self.legs[1].setAngles(leg2_array)
-        self.legs[2].setAngles(leg3_array)
-        self.legs[3].setAngles(leg4_array)
-        self.legs[4].setAngles(leg5_array)
-        self.legs[5].setAngles(leg6_array)
+        for i in range(6):
+            self.legs[i].setAngles(angle_array[i])
         
 if __name__ == "__main__":
     rospy.init_node("hexapod_controller")
